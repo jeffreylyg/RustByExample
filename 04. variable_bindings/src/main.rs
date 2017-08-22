95206a8932195728ce0093ebdc2fde2b07ed9d00
Yuguo Li <liyuguo.jeffrey@bytedance.com>
2017-08-22 18:51:46 +0800

    finish variable_bindings

---
+++ 04. variable_bindings/src/main.rs
@@ -0,0 +1,72 @@
+fn main() {
+    mutability(); //4.1
+    println!("");
+    scope_and_shadowing(); //4.2
+    println!("");
+    declare_first() //4.3
+}
+
+fn mutability() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+}
+
+fn scope_and_shadowing() {
+    // This binding lives in the main function
+    let long_lived_binding = 1;
+
+    // This is a block, and has a smaller scope than the main function
+    {
+        // This binding only exists in this block
+        let short_lived_binding = 2;
+
+        println!("inner short: {}", short_lived_binding);
+
+        // This binding *shadows* the outer one
+        let long_lived_binding = 5_f32;
+
+        println!("inner long: {}", long_lived_binding);
+    }
+    // End of the block
+
+    // Error! `short_lived_binding` doesn't exist in this scope
+    // println!("outer short: {}", short_lived_binding);
+    // FIXME ^ Comment out this line
+
+    println!("outer long: {}", long_lived_binding);
+
+    // This binding also *shadows* the previous binding
+    let long_lived_binding = 'a';
+
+    println!("outer long: {}", long_lived_binding);
+}
+
+fn declare_first() {
+    // Declare a variable binding
+    let a_binding;
+
+    {
+        let x = 2;
+
+        // Initialize the binding
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // Error! Use of uninitialized binding
+    // println!("another binding: {}", another_binding);
+    // FIXME ^ Comment out this line
+
+    another_binding = 1;
+
+    println!("another binding: {}", another_binding);
+}
