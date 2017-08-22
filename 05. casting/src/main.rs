8d5d6b5e5811745f83be686c2ea41bdcbe182317
Yuguo Li <liyuguo.jeffrey@bytedance.com>
2017-08-22 19:00:19 +0800

    finish casting

---
+++ 05. casting/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    literals(); //5.1
+    println!("");
+    inference(); //5.2
+    println!("");
+    alias(); //5.3
+}
+
+fn literals() {
+    let x = 1u8;
+    let y = 2u32;
+    let z = 3f32;
+
+    let i = 1;
+    let f = 1.0;
+
+    println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
+    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
+}
+
+fn inference() {
+     // Because of the annotation, the compiler knows that `elem` has type u8.
+    let elem = 5u8;
+
+    // Create an empty vector (a growable array).
+    let mut vec = Vec::new();
+    // At this point the compiler doesn't know the exact type of `vec`, it
+    // just knows that it's a vector of something (`Vec<_>`).
+
+    // Insert `elem` in the vector.
+    vec.push(elem);
+    // Aha! Now the compiler knows that `vec` is a vector of `u8`s (`Vec<u8>`)
+    // TODO ^ Try commenting out the `vec.push(elem)` line
+
+    println!("{:?}", vec);
+}
+
+fn alias() {
+    type NanoSecond = u64;
+    type Inch = u64;
+
+    #[allow(non_camel_case_types)]
+    type u64_t = u64;
+
+    // `NanoSecond` = `Inch` = `u64_t` = `u64`.
+    let nanoseconds: NanoSecond = 5 as u64_t;
+    let inches: Inch = 2 as u64_t;
+
+    // Note that type aliases *don't* provide any extra type safety, because
+    // aliases are *not* new types
+    println!("{} nanoseconds + {} inches = {} unit?",
+             nanoseconds,
+             inches,
+             nanoseconds + inches);
+}
