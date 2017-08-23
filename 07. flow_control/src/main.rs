6ce96f7d0059a1ecabaa37631c5e461dca154c89
Yuguo Li <liyuguo.jeffrey@bytedance.com>
2017-08-23 18:17:58 +0800

    finish flow_control

---
+++ 07. flow_control/src/main.rs
@@ -0,0 +1,384 @@
+fn main() {
+    if_else(); //7.1
+    println!("");
+    _loop(); //7.2
+    println!("");
+    loop_nesting_and_labels(); //7.2.1
+    println!("");
+    loop_returning_from_loops(); //7.2.2
+    println!("");
+    _while(); //7.3
+    println!("");
+    for_and_range(); //7.4
+    println!("");
+    _match(); //7.5
+    println!("");
+    match_destructuring_tuples(); //7.5.1.1
+    println!("");
+    match_destructuring_enums(); //7.5.1.2
+    println!("");
+    match_destructuring_pointers_ref(); //7.5.1.3
+    println!("");
+    match_destructuring_structs(); //7.5.1.4
+    println!("");
+    match_destructuring_guards(); //7.5.2
+    println!("");
+    match_destructuring_binding(); //7.5.3
+    println!("");
+    if_let(); //7.6
+    println!("");
+    while_let();
+}
+
+fn if_else() {
+    let n = 5;
+
+    if n < 0 {
+        println!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    let big_n =
+        if n < 10 && n > -10 {
+            println!(", and is a small number, increase ten-fold");
+
+            // This expression returns an `i32`.
+            10 * n
+        } else {
+            println!(", and is a big number, reduce by two");
+
+            // This expression must return an `i32` as well.
+            n / 2
+            // TODO ^ Try suppressing this expression with a semicolon.
+        };
+    //   ^ Don't forget to put a semicolon here! All `let` bindings need it.
+
+    println!("{} -> {}", n, big_n);
+}
+
+fn _loop() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    // Infinite loop
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // Skip the rest of this iteration
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            // Exit this loop
+            break;
+        }
+    }
+}
+
+fn loop_nesting_and_labels() {
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}
+
+fn loop_returning_from_loops() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2
+        }
+    };
+
+    println!("result: {}", result);
+}
+
+fn _while() {
+    let mut n = 1;
+
+    while n < 101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        n += 1;
+    }   
+}
+
+fn for_and_range() {
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("applebanana");
+        } else if n % 3 == 0 {
+            println!("apple");
+        } else if n % 5 == 0 {
+            println!("banana");
+        } else { 
+            println!("{}", n);
+        }
+    }
+}
+
+fn _match() {
+    let number = 13;
+    println!("Tell me about {}", number);
+
+    match number {
+        // Match a single value
+        1 => println!("One"),
+        // Match several values
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
+        // Match an inclusive range
+        13...19 => println!("A teen"),
+        // Handle the rest of cases
+        _ => println!("Ain't special"),
+    }
+
+    let boolean = true;
+    // Match is an expression too
+    let binary = match boolean {
+        // The arms of a match must cover all the possible values
+        false => 0,
+        true => 1,
+        // TODO ^ Try commenting out one of these arms
+    };
+
+    println!("{} -> {}", boolean, binary);
+}
+
+fn match_destructuring_tuples() {
+    let pair = (0, -2);
+
+    println!("Tell me about {:?}", pair);
+    // Match can be used to destructure a tuple
+    match pair {
+        // Destructure the second
+        (0, y) => println!("First is `0` and `y` is `{:?}`", y),
+        (x, 0) => println!("`x` is `{:?}` and last is `0`", x),
+        _      => println!("It doesn't matter what they are"),
+        // `_` means don't bind the value to a variable
+    }
+}
+
+fn match_destructuring_enums() {
+    enum Color {
+        // These 3 are specified solely by their name.
+        Red,
+        Blue,
+        Green,
+        // These likewise tie `u32` tuples to different names: color models.
+        RGB(u32, u32, u32),
+        HSV(u32, u32, u32),
+        HSL(u32, u32, u32),
+        CMY(u32, u32, u32),
+        CMYK(u32, u32, u32, u32),
+    }
+
+    let color = Color::RGB(122, 17, 40);
+    // TODO ^ Try different variants for `color`
+
+    println!("What color is it?");
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red   => println!("The color is Red!"),
+        Color::Blue  => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) =>
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+                c, m, y, k),
+        // Don't need another arm because all variants have been examined
+    }
+}
+
+fn match_destructuring_pointers_ref() {
+    // Assign a reference of type `i32`. The `&` signifies there
+    // is a reference being assigned.
+    let reference = &4;
+    println!("reference: {}", reference);
+
+    match reference {
+        // If `reference`s is pattern matched against `&val`, it results
+        // in a comparison like:
+        // `&i32`
+        // `&val`
+        // ^ We see that if the matching `&`s are dropped, then the `i32`
+        // should be assigned to `val`.
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    match *reference {
+        val => println!("Got a value via dereferencing: {:?}", val),
+    }
+
+    // What if you don't start with a reference? `reference` was a `&`
+    // because the right side was already a reference. This is not
+    // a reference because the right side is not one.
+    let _not_a_reference = 3;
+    println!("_not_a_reference: {}", _not_a_reference);
+
+    // Rust provides `ref` for exactly this purpose. It modifies the
+    // assignment so that a reference is created for the element; this
+    // reference is assigned.
+    let ref _is_a_reference = 3;
+    println!("_is_a_reference: {}", _is_a_reference);
+
+     // Accordingly, by defining 2 values without references, references
+    // can be retrieved via `ref` and `ref mut`.
+    let value = 5;
+    let mut mut_value = 6;
+
+    // Use `ref` keyword to create a reference.
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    // Use `ref mut` similarly.
+    match mut_value {
+        ref mut m => {
+            // Got a reference. Gotta dereference it before we can
+            // add anything to it.
+            *m += 10;
+            println!("We added 10. `mut_value`: {:?}", m);
+        },
+    }
+}
+
+fn match_destructuring_structs() {
+    struct Foo { x: (u32, u32), y: u32 }
+
+    // destructure members of the struct
+    let foo = Foo { x: (1, 2), y: 3 };
+    let Foo { x: (a, b), y } = foo;
+
+    println!("a = {}, b = {},  y = {} ", a, b, y);
+
+    // you can destructure structs and rename the variables,
+    // the order is not important
+
+    let Foo { x: i, y: j } = foo;
+    println!("i = {:?}, j = {:?}", i, j);
+
+    // and you can also ignore some variables:
+    let Foo { y, .. } = foo;
+    println!("y = {}", y);
+}
+
+fn match_destructuring_guards() {
+    let pair = (2, -2);
+    println!("Tell me about {:?}", pair);
+
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, y) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
+}
+
+/**
+ * Indirectly accessing a variable makes it impossible to branch and use that variable without re-binding. 
+ * match provides the @ sigil for binding values to names:
+ */
+fn match_destructuring_binding() {
+    fn age() -> u32 {
+        9
+    }
+
+    println!("Tell me type of person you are");
+
+    match age() {
+        0             => println!("I'm not born yet I guess"),
+        // Could `match` 1 ... 12 directly but then what age
+        // would the child be? Instead, bind to `n` for the
+        // sequence of 1 .. 12. Now the age can be reported.
+        n @ 1  ... 12 => println!("I'm a child of age {:?}", n),
+        n @ 13 ... 19 => println!("I'm a teen of age {:?}", n),
+        // Nothing bound. Return the result.
+        n             => println!("I'm an old person of age {:?}", n),
+    }
+}
+
+fn if_let() {
+    let number = Some(7);
+    let letter: Option<i32> = None;
+    let emoticon: Option<i32> = None;
+
+    if let Some(i) = number {
+        println!("Matched {:?}!", i);
+    }
+
+    if let Some(i) = letter {
+        println!("Matched {:?}!", i);
+    } else {
+        println!("Didn't match a number. Let's go with a letter!");
+    }
+
+    // Provide an altered failing condition.
+    let i_like_letters = false;
+
+    if let Some(i) = emoticon {
+        println!("Matched {:?}!", i);
+    // Destructure failed. Evaluate an `else if` condition to see if the
+    // alternate failure branch should be taken:
+    } else if i_like_letters {
+        println!("Didn't match a number. Let's go with a letter!");
+    } else {
+        // The condition evaluated false. This branch is the default:
+        println!("I don't like letters. Let's go with an emoticon :)!");
+    }
+}
+
+fn while_let() {
+    let mut optional = Some(0);
+
+    while let Some(i) = optional {
+        if i > 9 {
+            println!("Greater than 9, quit!");
+            optional = None;
+        } else {
+            println!("`i` is `{:?}`. Try again.", i);
+            optional = Some(i + 1);
+        }
+    }
+}
